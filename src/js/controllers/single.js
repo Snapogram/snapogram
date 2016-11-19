0b8d08878b0b20f684cf56d29cbde41771d7b495
Tai Chen <mindlikewater@gmail.com>
2016-11-19 13:55:21 -0500

    Getting details page working

--- src/js/controllers/single.js
+++ src/js/controllers/single.js
@@ -1,17 +1,18 @@
 import { SERVER } from "../server";
 
-function DetailsController ($scope, $http, $stateParams) {
-  // $scope.pic = {};
-  //
-  // function init () {
-  //   let url = SERVER + $stateParams.id;
-  //   $http.get(url).then((resp) => {
-  //     $scope.pic = resp.data;
-  //   });
-  // };
-  //
-  // init();
+function DetailsController ($scope, $http, $stateParams, $document) {
+  $scope.pic = {};
+
+  function init () {
+    let url = SERVER + 'images/' + $stateParams.id;
+    $http.get(url).then((resp) => {
+      $scope.pic = resp.data;
+    });
+  };
+
+  init();
+
 }
 
-DetailsController.$inject = ['$scope', '$http', '$stateParams'];
+DetailsController.$inject = ['$scope', '$http', '$stateParams', '$document'];
 export { DetailsController };
